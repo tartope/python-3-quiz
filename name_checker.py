458bbf09abc7ae87821f2013737614441597ccfa
Tunisia Artope <tartope@xpanxion.com>
2023-07-10 10:58:50 -0400

    Solution

--- name_checker.py
+++ name_checker.py
@@ -1,7 +1,18 @@
 from ValidationException import ValidationException
 
 def validate_file(file_path):
-    pass #TODO: Only add code inside this function.
+    with open(file_path, "r") as file:
+        next(file)
+        for lines in file:
+            line = lines.split(',')
+            # print(line)
+            f_name = line[0]
+            # print(f_name)
+            try:
+                f_name.isalpha()
+            except:
+                raise ValidationException(f"Invalid first name: '{f_name}'")
+    # pass #TODO: Only add code inside this function.
 
 
 def test():
